33c74c09bfb91eda36acd22a5a9f9d899df51763
cy0423 <cy0423@yeah.net>
2017-12-06 09:15:45 +0800

    20171206-1

---
+++ 2/vfm-admin/users/users.php
@@ -0,0 +1,16 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$VNDlu4qS$i1NRwBRrEKHlxM0B43K1H.',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'cy',
+    'pass' => '$1$UY2pt90X$7EFrfrPgqgNP6l4FdpR04.',
+    'role' => 'superadmin',
+  ),
+);
